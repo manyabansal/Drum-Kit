34867400c9e1ce0e451756b6b018a0335600baa9
Manya Bansal <98588311+manyabansal@users.noreply.github.com>
2022-08-02 00:21:31 +0530

    changes for key press in js file

--- index.js
+++ index.js
@@ -2,42 +2,60 @@ var audio;
 
 for(var i=0; i<document.querySelectorAll(".drum").length; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
 
-  switch (this.innerHTML) {
-    case "w":
-    var audio= new Audio("sounds/tom-1.mp3");
+}
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+
+switch (key) {
+  case "w":
+  var audio= new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+  case "a":
+audio= new Audio("sounds/tom-2.mp3");
       audio.play();
       break;
 
-    case "a":
-  audio= new Audio("sounds/tom-2.mp3");
+  case "s":
+audio= new Audio("sounds/tom-3.mp3");
         audio.play();
         break;
 
-    case "s":
-  audio= new Audio("sounds/tom-3.mp3");
+  case "d":
+  audio= new Audio("sounds/tom-4.mp3");
           audio.play();
           break;
 
-    case "d":
-    audio= new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
+  case "j":
+  audio= new Audio("sounds/snare.mp3");
+         audio.play();
+         break;
 
-    case "j":
-    audio= new Audio("sounds/snare.mp3");
-           audio.play();
-           break;
+  case "k":
+  audio= new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
 
-    case "k":
-    audio= new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
+  case "l":
+  audio= new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+}
+}
 
-    case "l":
-    audio= new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-  }
-  });
+function buttonAnimation(key){
+  document.querySelector("." + key).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+ key).classList.remove("pressed");
+  },100);
 }
